8197f0572a4c652556107ec5d8837a68687d4161
Jonathan Vega <jonathan.alor@gmail.com>
2018-04-29 21:35:04 -0500

    Se realiza cambios y se deja la version final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ANIMALES } from '../../data/data.animales';
 import { Animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,17 +10,50 @@ import { Animal } from '../../interfaces/animal.interface';
 })
 export class HomePage {
 
-  animales:Animal[] = [];
+  animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando: boolean = false;
+
   constructor(public navCtrl: NavController) {
-    this.animales = ANIMALES.splice(0)
+    this.animales = ANIMALES.slice(0)
   }
 
-  reproducir(animal:Animal){
-    let audio = new Audio()
-    audio.src = animal.audio
-    audio.load()
-    audio.play()
+  reproducir(animal: Animal) {
+    this.pausar_audio(animal);
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+    this.audio.src = animal.audio
+    this.audio.load()
+    this.audio.play()
     animal.reproduciendo = true
-    setTimeout(() => animal.reproduciendo = false , animal.duracion * 1000) 
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000)
+  }
+
+  private pausar_audio(animalSel: Animal) {
+    clearTimeout(this.audioTiempo);
+    this.audio.currentTime = 0;
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrar_animal(idx: number) {
+    this.animales.splice(idx, 1)
+  }
+
+  recargar_animales(refresher: Refresher) {
+    setTimeout(() => {
+      this.animales = ANIMALES.slice(0)
+      refresher.complete()
+    }, 2000)
+  }
+
+  reordenar_animales(indices: any) {
+    this.animales = reorderArray(this.animales, indices);
   }
 }
